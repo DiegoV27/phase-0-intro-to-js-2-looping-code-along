d9a7989b7b3974b56d97f590f7d57187412998c7
Diego Valero <diego27jay@gmail.com>
2021-12-15 15:21:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    let newArray = [];
+    for(let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray;
+}
+
+ function countDown(num) {
+    while(num >= 0) {
+        console.log(num)
+    num --;
+    }
+}
